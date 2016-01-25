18ac0346fc190d6238e0f4c138b6b065db04ead4
Emiliano Torre <e.torre@fz-juelich.de>
2016-01-25 15:04:46 +0100

    Added unittests for ASSET analysis

---
+++ elephant/test/test_asset.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 22 14:35:49 2014
+
+@author: quaglio
+"""
+
+import unittest
+import numpy as np
+import scipy
+import jelephant.analysis.worms as worms
+
+
+stretchedmetric2d = worms.__stretched_metric_2d
+cluster = worms.cluster
+
+
+class WormsTestCase(unittest.TestCase):
+
+    def test_stretched_metric_2d_size(self):
+        nr_points = 4
+        x = np.arange(nr_points)
+        D = stretchedmetric2d(x, x, stretch=1, ref_angle=45)
+        self.assertEqual(D.shape, (nr_points, nr_points))
+
+    def test_stretched_metric_2d_correct_stretching(self):
+        x = (0, 1, 0)
+        y = (0, 0, 1)
+        stretch = 10
+        ref_angle = 0
+        D = stretchedmetric2d(x, y, stretch=stretch, ref_angle=ref_angle)
+        self.assertEqual(D[0, 1], 1)
+        self.assertEqual(D[0, 2], stretch)
+
+    def test_stretched_metric_2d_symmetric(self):
+        x = (1, 2, 2)
+        y = (1, 2, 0)
+        stretch = 10
+        D = stretchedmetric2d(x, y, stretch=stretch, ref_angle=45)
+        np.testing.assert_array_almost_equal(D, D.T, decimal=12)
+
+    def test_stretched_metric_2d_equals_euclidean_if_stretch_1(self):
+        x = np.arange(10)
+        y = y = x ** 2 - 2 * x - 4
+        # compute stretched distance matrix
+        stretch = 1
+        D = stretchedmetric2d(x, y, stretch=stretch, ref_angle=45)
+        # Compute Euclidean distance matrix
+        points = np.vstack([x, y]).T
+        E = scipy.spatial.distance_matrix(points, points)
+        # assert D == E
+        np.testing.assert_array_almost_equal(D, E, decimal=12)
+
+    def test_cluster_correct(self):
+        mat = np.zeros((6, 6))
+        mat[[2, 4, 5], [0, 0, 1]] = 1
+        mat_clustered = cluster(mat, eps=4, min=2, stretch=6)
+
+        mat_correct = np.zeros((6, 6))
+        mat_correct[[4, 5], [0, 1]] = 1
+        mat_correct[2, 0] = -1
+        np.testing.assert_array_equal(mat_clustered, mat_correct)
+
+    def test_cluster_symmetric(self):
+        x = [0, 1, 2, 5, 6, 7]
+        y = [3, 4, 5, 1, 2, 3]
+        mat = np.zeros((10, 10))
+        mat[x, y] = 1
+        mat = mat + mat.T
+        # compute stretched distance matrix
+        mat_clustered = cluster(mat, eps=4, min=2, stretch=6)
+        mat_equals_m1 = (mat_clustered == -1)
+        mat_equals_0 = (mat_clustered == 0)
+        mat_larger_0 = (mat_clustered > 0)
+        np.testing.assert_array_equal(mat_equals_m1, mat_equals_m1.T)
+        np.testing.assert_array_equal(mat_equals_0, mat_equals_0.T)
+        np.testing.assert_array_equal(mat_larger_0, mat_larger_0.T)
+
+    def test_sse_difference(self):
+        a = {(1,2): set([1,2,3]), (3,4): set([5,6]), (6,7): set([0,1])}
+        b = {(1,2): set([1,2,5]), (5,6): set([0,2]), (6,7): set([0,1])}
+        diff_ab_pixelwise = {(3, 4): set([5, 6])}
+        diff_ba_pixelwise = {(5, 6): set([0, 2])}
+        diff_ab_linkwise = {(1, 2): set([3]), (3, 4): set([5, 6])}
+        diff_ba_linkwise = {(1, 2): set([5]), (5, 6): set([0, 2])}
+        self.assertEqual(
+            worms.sse_difference(a, b, 'pixelwise'), diff_ab_pixelwise)
+        self.assertEqual(
+            worms.sse_difference(b, a, 'pixelwise'), diff_ba_pixelwise)
+        self.assertEqual(
+            worms.sse_difference(a, b, 'linkwise'), diff_ab_linkwise)
+        self.assertEqual(
+            worms.sse_difference(b, a, 'linkwise'), diff_ba_linkwise)
+
+    def test_sse_intersection(self):
+        a = {(1,2): set([1,2,3]), (3,4): set([5,6]), (6,7): set([0,1])}
+        b = {(1,2): set([1,2,5]), (5,6): set([0,2]), (6,7): set([0,1])}
+        inters_ab_pixelwise = {(1, 2): set([1, 2, 3]), (6, 7): set([0, 1])}
+        inters_ba_pixelwise = {(1, 2): set([1, 2, 5]), (6, 7): set([0, 1])}
+        inters_ab_linkwise = {(1, 2): set([1, 2]), (6, 7): set([0, 1])}
+        inters_ba_linkwise = {(1, 2): set([1, 2]), (6, 7): set([0, 1])}
+        self.assertEqual(
+            worms.sse_intersection(a, b, 'pixelwise'), inters_ab_pixelwise)
+        self.assertEqual(
+            worms.sse_intersection(b, a, 'pixelwise'), inters_ba_pixelwise)
+        self.assertEqual(
+            worms.sse_intersection(a, b, 'linkwise'), inters_ab_linkwise)
+        self.assertEqual(
+            worms.sse_intersection(b, a, 'linkwise'), inters_ba_linkwise)
+
+    def test_sse_relations(self):
+        a = {(1,2): set([1,2,3]), (3,4): set([5,6]), (6,7): set([0,1])}
+        b = {(1,2): set([1,2,5]), (5,6): set([0,2]), (6,7): set([0,1])}
+        c = {(5,6): set([0,2])}
+        d = {(3,4): set([0,1]), (5,6): set([0,1,2])}
+        self.assertTrue(worms.sse_isequal({}, {}))
+        self.assertTrue(worms.sse_isequal(a, a))
+        self.assertFalse(worms.sse_isequal(b, c))
+        self.assertTrue(worms.sse_isdisjoint(a, c))
+        self.assertTrue(worms.sse_isdisjoint(a, d))
+        self.assertFalse(worms.sse_isdisjoint(a, b))
+        self.assertTrue(worms.sse_issub(c, b))
+        self.assertTrue(worms.sse_issub(c, d))
+        self.assertFalse(worms.sse_issub(a, b))
+        self.assertTrue(worms.sse_issuper(b, c))
+        self.assertTrue(worms.sse_issuper(d, c))
+        self.assertFalse(worms.sse_issuper(a, b))
+        self.assertTrue(worms.sse_overlap(a, b))
+        self.assertFalse(worms.sse_overlap(c, d))
+
+
+def suite():
+    suite = unittest.makeSuite(WormsTestCase, 'test')
+    return suite
+
+
+def run():
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite())
+
+
+if __name__ == "__main__":
+    unittest.main()
+    # runner = unittest.TextTestRunner(verbosity=2)
+    # runner.run(suite())
